b71a1bcfff5c6ee91f9c6ffdd3103fdb988c152b
luisflorenzan <86024563+luisflorenz@users.noreply.github.com>
2024-08-07 13:46:18 +0000

    commit all codes

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+//require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_GOERLI_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
+};
